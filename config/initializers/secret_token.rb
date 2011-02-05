12a0acf0223f98cb73177fca51a9edaf97ac4363
Chris Searle <chris@chrissearle.org>
2011-02-05 19:48:09 +0100

    Blank rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwphchArchive::Application.config.secret_token = '1f8f218233316122bf9f2459f99f791ee65cfb9ff9080d2f4ee587a3088fff245903770aa2c4bfe2a184c8f5f555c42f478ef683fbd7ab0a84b87c5ff997ba55'
