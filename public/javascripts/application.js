9ea0fa881103367f5b864ec20cc28da6951c5b0f
Chris Searle <chris@chrissearle.org>
2011-02-11 14:28:19 +0100

    AJAX page change only for historyState enabled viewers.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,12 +2,6 @@
 // This file is automatically included by javascript_include_tag :defaults
 
 $(function () {
-    $('.ajaxable').live('click', function (e) {
-        $('div.info_div').html('');
-        $.getScript(this.href);
-        e.preventDefault();
-    });
-
     $('a.colour_changer').click(function(e) {
         var href = $('#colour_css_tag').attr('href');
         href = href.replace(/[^\/]*css/, $(this).data('colour') + ".css");
@@ -15,19 +9,24 @@ $(function () {
         e.preventDefault();
     });
 
-    $('#loading').hide().ajaxStart(function() {
-//        offset = $('#main').offset();
+    if (!!window.history && !!window.history.pushState) {
+        $('.ajaxable').live('click', function (e) {
+            history.pushState(null, "", this.href);
+            $('div.info_div').html('');
+            $.getScript(this.href);
+            e.preventDefault();
+        });
 
-  //      $(this).offset(offset);
-    //    $(this).width($('#main').width());
-      //  $(this).height($(window).height());
-        $(this).show();
-    }).ajaxStop(function() {
-        $(this).hide();
-    });
+        $('#loading').hide().ajaxStart(
+                                      function() {
+                                          $(this).show();
+                                      }).ajaxStop(function() {
+            $(this).hide();
+        });
 
-//    $(window).bind("popstate", function () {
-//        $.getScript(location.href);
-//    });
+        $(window).bind("popstate", function () {
+            $.getScript(location.href);
+        });
+    }
 });
 
